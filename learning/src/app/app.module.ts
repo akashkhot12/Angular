40e095148bcb6941f616a876551a0e48ff3f0b50
Akash khot <akashkhot03@gmail.com>
2023-05-27 11:33:31 +0530

    cheaking component

--- learning/src/app/app.module.ts
+++ learning/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
