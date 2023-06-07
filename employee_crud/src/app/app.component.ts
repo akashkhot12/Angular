9758398b095b96fe1f38799b1f9bef52c93bae25
Akash khot <akashkhot03@gmail.com>
2023-06-07 10:50:52 +0530

    adding option for education

--- employee_crud/src/app/app.component.ts
+++ employee_crud/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee_crud';
+
+  educationOptions =[
+    '10th pass',
+    'diploma',
+    'graduate',
+    'post graduate',
+    'phD',
+  ];
+
+  ngAfterViewInIt():void{
+    // this.buttontemp.nativeElement.click();
+  }
 }
