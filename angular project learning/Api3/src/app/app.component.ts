c2134bceb670af5385162c9d33e095753fd7eb46
akashkhot12 <akashkhot03@gmail.com>
2023-07-21 12:43:44 +0530

    create a ui to print data

--- angular project learning/Api3/src/app/app.component.ts
+++ angular project learning/Api3/src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostmanTsService } from 'src/service/postman.ts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Api3';
   data:any[]=[]
 
-
+constructor(private ap:PostmanTsService){}
+  ngOnInit(): void {
+    this.getJson();
+    
+  }
 
   getJson(){
-    
+    this.ap.getJson().subscribe((res:any)=>{
+      this.data = res.data;
+      console.log(this.data);
+      
+    })
+
   }
 }
