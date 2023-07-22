1869b7d076df0fd55618c8fcf46eea829e0bdc1a
akashkhot12 <akashkhot03@gmail.com>
2023-07-22 14:40:29 +0530

    app component

--- getJson/src/app/app.component.ts
+++ getJson/src/app/app.component.ts
@@ -10,11 +10,8 @@ export class AppComponent implements OnInit {
   title = 'getJson';
   data:any[]=[];
   
-constructor(private EnvService:EnvService ){
-
-}
+constructor(private EnvService:EnvService ){}
   
-
   ngOnInit():void{
     this.getAppJSon()
   }
